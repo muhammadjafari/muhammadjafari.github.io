753398b85c31940b3ec2ff26980b16e6a38eb791
muhammadjafari <jafariimuhammad@gmail.com>
2022-02-07 00:53:58 +0330

    cv site added

---
+++ script/script.js
@@ -0,0 +1,110 @@
+
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-left: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+// portfolio
+function openColumn(evt, column) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(column).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// default open
+document.getElementById("defaultOpen").click();
+
+// modal 
+function closeModal() {
+    
+    var opacity = 0 ;
+    var intervalID = 0 ;
+    // window.onload = fadeout ;
+        function fadeout() {
+                setInterval(hide, 30)
+        }
+    function hide() {
+        var body=document.getElementById("modalid");
+        opacity =
+        Number(window.getComputedStyle(body).getPropertyValue("opacity"))
+            if(opacity>0) {
+                opacity = opacity-0.1;
+                body.style.opacity=opacity
+            }
+            else{
+                clearInterval(intervalID);
+                document.body.classList.add("close-modal-body");
+        document.getElementById("modalid").classList.add("close-modal-class");
+            }
+    }
+    fadeout();
+    
+        
+}
